d32a5f10fe792107e87d12adb62be9b3425fc203
Danish Safdariyan <ds968@cornell.edu>
2025-06-18 19:04:06 -0400

    build site into docs for GH-Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  base: "/my-portfolio/",
+  base: "/",
   plugins: [
     tailwindcss(),
   ],
